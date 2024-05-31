3714304208c2fbee02fc67113d8a6388d70b43fc
baner175 <baner175@umn.edu>
2024-05-31 15:16:42 -0500

    updating power and level simulation codes

--- New example/Analysis on full data/power and level simulation.R
+++ New example/Analysis on full data/power and level simulation.R
@@ -5,7 +5,7 @@ library(foreach)
 library(iterators)
 
 n_rep <- 1e4 # do it for 1e4
-n_samp <- 1e3
+n_samp <- 5e3
 
 # UNKNOWN DENSITIES:
 mean_back <- 0.5; sd_back <- 2.5; eta_true <- 0.03
@@ -17,7 +17,8 @@ f_mix <- function(t) eta_true*fs(t) + (1-eta_true)*fb(t)
 eta_seq <- seq(0, 0.06, 0.03)
 theta_vec <- c()
 t_stat_vec <- c()
-set.seed(12345)
+seed <- 12345
+set.seed(seed)
 
 numCores <- detectCores()
 cl <- makeSOCKcluster(numCores)
@@ -56,11 +57,16 @@ mean(result_eta[[1]][,1]>qnorm(0.95)) # 0.0077
 mean(result_eta[[2]][,1]>qnorm(0.95)) # 0.5904
 mean(result_eta[[3]][,1]>qnorm(0.95)) # 0.9939
 
-theta_vec_null <- result_eta[[1]][,2]
-(delta <- integrate(function(t) fb(t)*S1(t), l, u)$value)
-sd_theta <- (integrate(function(t) fb(t)*S1(t)^2, l, u)$value - delta^2)/sqrt(n_samp)
-hist(theta_vec_null, probability = TRUE, breaks = 50)
-true_dist <- function(t) dnorm(t, mean = delta, sd =sd_theta)
-curve(true_dist, add = TRUE, col = 'blue')
-abline(v = delta, col = 'red', lty = 2)
+(filename <- paste0('nrep_',n_rep, '__nsamp_', n_samp, '__seed_',seed,'.csv'))
 
+levels <- sapply(result_eta, function(x) mean(x[,1] > qnorm(0.95)))
+res_df <- data.frame(eta = eta_seq, levels = levels)
+write.csv(res_df, file = file.path('simulation_results/', filename), row.names = FALSE)
+
+# theta_vec_null <- result_eta[[1]][,2]
+# (delta <- integrate(function(t) fb(t)*S1(t), l, u)$value)
+# sd_theta <- (integrate(function(t) fb(t)*S1(t)^2, l, u)$value - delta^2)/sqrt(n_samp)
+# hist(theta_vec_null, probability = TRUE, breaks = 50)
+# true_dist <- function(t) dnorm(t, mean = delta, sd =sd_theta)
+# curve(true_dist, add = TRUE, col = 'blue')
+# abline(v = delta, col = 'red', lty = 2)
