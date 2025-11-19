67c1a10859e1413f26b1445fbb6bc13ef1d75eb5
baner175 <baner175@umn.edu>
2025-11-18 19:28:27 -0600

    Adding codes for new wobkg simulation setup

---
+++ simulations/power_sim_wobkg_beta_est_new.R
@@ -0,0 +1,154 @@
+rm(list = ls())
+library(truncdist)
+## Global parameters ##
+
+#parameters for the signal
+mean_sig <- 1.28
+sd_sig <- 0.02
+
+l <- 1; u <- 2
+
+n_bins <- c(30, 50, 100)
+
+# signal density and CDF
+fs <- function(x, mean = mean_sig) dtrunc(x, a = l, b = u, spec = 'norm', mean = mean, sd = sd_sig)
+Fs <- function(x) ptrunc(x, a = l, b = u, spec = 'norm', mean = mean_sig, sd = sd_sig)
+
+#parameter for the true background
+bkg_rate <- 3.3; bkg_shape <- 0.5
+
+# true bkg density
+fb_true <- function(x) dtrunc(x, a = l, b = u, spec = 'gamma',
+                              rate = bkg_rate, shape = bkg_shape)
+
+source('functions_for_power_simulation_wobkg.R')
+
+library(optparse)
+
+option_list <- list(
+  make_option(c("-B", "--N_iter"), type = "integer", default = 1e4,
+              help = "Number of Iterations", metavar = "number"),
+  make_option(c('-n', '--n_phys'), type = "integer", default = 2e3,
+              help = 'physics sample size', metavar = "number"),
+  make_option(c('-l', '--lambda'), type = "double", default = 0,
+              help = 'lambda in gb', metavar = "number"),
+  make_option(c('-e', '--eta'), type = "double", default = 0,
+              help = 'proportion of signal', metavar = "number"),
+  make_option('--eps', type = "double", default = 1e-3,
+              help = 'mass outside of the signal region', metavar = "number")
+)
+
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+B <- as.numeric(opt$N_iter); n_phys <- as.numeric(opt$n_phys)
+eta <- as.numeric(opt$eta); lambda <- as.numeric(opt$lambda)
+eps <- as.numeric(opt$eps)
+  
+# Calculating signal region:
+
+# Figuring out (mu_s -d, mu_s + d) that covers an area of 1-eps:
+find_d <- function(d)
+{
+  pl <- Fs(mean_sig - d)
+  pu <- Fs(mean_sig + d)
+  return(pu-pl-1+eps)
+}
+
+sol <- uniroot(find_d, lower = 0, upper = min(mean_sig - l,u - mean_sig))
+
+r <- sol$root
+
+M_lower <- mean_sig - r
+M_upper <- mean_sig + r
+
+# PROPOSAL BACKGROUND DENSITY PARAMETERS:
+mean1_in_gb <- 0.5*M_lower + 0.5*mean_sig
+mean2_in_gb <- 0.4*M_upper + 0.6*mean_sig
+sd_in_gb <- 4*sd_sig
+
+# simulation results with known beta:
+
+binned_res <- sapply(n_bins, function(k){
+  simulated_power_binned(eta = eta, nbins = k, 
+                         T_phys = n_phys, 
+                         lambda = lambda,
+                         mean1_in_gb = mean1_in_gb, 
+                         mean2_in_gb = mean2_in_gb,
+                         sd_in_gb = sd_in_gb, 
+                         nsims = B, seed = 12345,
+                         signif.level = 0.05)
+})
+
+unbinned_res <- simulated_power_unbinned(eta = eta, n_phys = n_phys, 
+                                         lambda = lambda,
+                                         mean1_in_gb = mean1_in_gb, 
+                                         mean2_in_gb = mean2_in_gb,
+                                         sd_in_gb = sd_in_gb, 
+                                         nsims = B, seed = 12345,
+                                         signif.level = 0.05)
+neg_exp_log_lik <- function(beta)
+{
+  -integrate(function(x){
+    fs_val1 <- dtrunc(x, mean = mean1_in_gb, sd = sd_in_gb,
+                      a = l, b = u,
+                      spec = 'norm')
+    fs_val2 <-  dtrunc(x, mean = mean2_in_gb, sd = sd_in_gb,
+                       a = l, b = u,
+                       spec = 'norm')
+    qb <- dtrunc(x, spec = 'pareto', a = l, b = u,
+                 scale = l, shape = beta)
+    gb <- lambda*(fs_val1+fs_val2) + (1-2*lambda)*qb
+    
+    return(log(gb)*fb_true(x))
+  }, l, u)$value
+}
+neg_exp_log_lik <- Vectorize(neg_exp_log_lik)
+
+beta_star <- nlminb(start = 0.01, 
+                   objective = neg_exp_log_lik,
+                   lower = 1e-6,
+                   upper = Inf)$par
+normS <- integrate(function(x){
+  fs <- fs(x)
+  fs_val1 <- dtrunc(x, mean = mean1_in_gb, sd = sd_in_gb,
+                    a = l, b = u,
+                    spec = 'norm')
+  fs_val2 <-  dtrunc(x, mean = mean2_in_gb, sd = sd_in_gb,
+                     a = l, b = u,
+                     spec = 'norm')
+  qb <- dtrunc(x, spec = 'pareto', a = l, b = u,
+               scale = l, shape = beta_star)
+  gb <- lambda*(fs_val1+fs_val2) + (1-2*lambda)*qb
+  S <- (fs/gb - 1)
+  return((S^2)*gb)
+}, l, u)$value |> sqrt()
+delta_val <- integrate(function(x){
+  fs <- fs(x)
+  fs_val1 <- dtrunc(x, mean = mean1_in_gb, sd = sd_in_gb,
+                    a = l, b = u,
+                    spec = 'norm')
+  fs_val2 <-  dtrunc(x, mean = mean2_in_gb, sd = sd_in_gb,
+                     a = l, b = u,
+                     spec = 'norm')
+  qb <- dtrunc(x, spec = 'pareto', a = l, b = u,
+               scale = l, shape = beta_star)
+  gb <- lambda*(fs_val1+fs_val2) + (1-2*lambda)*qb
+  S1 <- (fs/gb-1)/normS
+  return(S1*fb_true(x))
+}, l, u)$value
+
+power_res <- data.frame(t(c(binned_res, unbinned_res, delta_val)))
+colnames(power_res) <- c(paste0(c(paste0(n_bins, ' - bins'), 'unbinned'), ' (beta known)'),
+                         'delta_star')
+file_name <- paste0('/home/baner175/Desktop/background_modeling/simulations/',
+                    'Results/WOBKG__',
+                    'beta_estimated_',
+                    'eta(',eta,')_',
+                    'lambda(', lambda,')_',
+                    'n_bins(',paste0(n_bins, collapse = '-'),')_',
+                    'B(',B,')_',
+                    'n_phys(',n_phys,').csv')
+
+write.csv(x = power_res, file_name,
+          row.names = FALSE)
---
+++ simulations/wobkg_sim_setup_new.R
@@ -0,0 +1,93 @@
+rm(list = ls())
+library(truncdist)
+library(VGAM)
+library(doSNOW)
+library(parallel)
+library(foreach)
+library(optparse)
+library(latex2exp)
+l <- 1; u <- 2
+#parameters for the signal
+mean_sig <- 1.28
+sd_sig <- 0.02
+
+eps <- 1e-3; beta <- 3.87
+
+# signal density
+fs <- function(x, mean = mean_sig) dtrunc(x, a = l, b = u, spec = 'norm', mean = mean, sd = sd_sig)
+Fs <- function(x) ptrunc(x, a = l, b = u, spec = 'norm', mean = mean_sig, sd = sd_sig)
+
+# Figuring out (mu_s -d, mu_s + d) that covers an area of 1-eps:
+find_d <- function(d)
+{
+  pl <- Fs(mean_sig-d)
+  pu <- Fs(mean_sig+d)
+  return(pu-pl-1+eps)
+}
+sol <- uniroot(find_d, lower = 0, upper = min(mean_sig - l,u - mean_sig))
+r <- sol$root
+M_lower <- mean_sig - r; M_upper <- mean_sig + r
+
+#parameter for the true background
+bkg_rate <- 3.3; bkg_shape <- 0.5
+
+# true bkg density
+fb_true <- function(x) dtrunc(x, a = l, b = u, spec = 'gamma',
+                              rate = bkg_rate, shape = bkg_shape)
+qb <- function(x){
+  dtrunc(x, spec = 'pareto', a = l, b = u,
+         scale = l, shape = beta0)
+}
+
+mean1_in_gb <- 0.5*M_lower + 0.5*mean_sig; sd_in_gb <- 4*sd_sig
+mean2_in_gb <- 0.4*M_upper + 0.6*mean_sig
+
+gb <- function(x, lambda) {
+  fs_val1 <- dtrunc(x, mean = mean1_in_gb, sd = sd_in_gb,
+                    a = l, b = u,
+                    spec = 'norm')
+  fs_val2 <-  dtrunc(x, mean = mean2_in_gb, sd = sd_in_gb,
+                     a = l, b = u,
+                     spec = 'norm')
+  qb <- dtrunc(x, spec = 'pareto', a = l, b = u,
+               scale = l, shape = beta)
+  
+  return(lambda*(fs_val1+fs_val2) + (1-2*lambda)*qb)
+}
+
+curve(fb_true, from = M_lower - 0.05, to = M_upper + 0.05, col = 'brown',
+      lwd = 2, lty = 1, ylab = '')
+lambda_seq <- seq(0, 0.025, length.out = 6)
+abline(v = c(M_lower, M_upper), lty = 3, lwd = 2,
+       col = 'black')
+
+mycols <- c('black', 'red', 'orange', 'green', 'blue', 'purple')
+for(i in 1:length(lambda_seq)){
+  curve(gb(x, lambda = lambda_seq[i]),
+        col = mycols[i],
+        lwd = 2, lty = i, add = TRUE)
+}
+delta_seq <- sapply(lambda_seq, function(lam){
+  normS <- integrate(function(x){
+    fs <- fs(x); gb <- gb(x, lambda = lam)
+    S <- (fs/gb - 1)
+    return((S^2)*gb)
+  }, l, u)$value |> sqrt()
+  delta <- integrate(function(x){
+    fs <- fs(x); gb <- gb(x, lambda = lam)
+    S1 <- (fs/gb - 1)/normS
+    return(S1*fb_true(x))
+  }, l, u)$value
+  return(delta)
+})
+legend('top',
+       legend = c(
+           TeX(
+             paste0(
+               sprintf('$g_b(\\lambda = %f);$', rev(lambda_seq)),
+               sprintf('   $\\delta = %.4f$', rev(delta_seq))
+               )
+           ),
+           TeX('$f_b(x)$')
+       ),
+       bty = 'n', lty = c(6:1, 1), col = c(rev(mycols), 'brown'), lwd = 2)
